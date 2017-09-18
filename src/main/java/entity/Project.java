ef797b2aff19334b2e13e7c194e5ccaf6bc80773
Jesus <deleterr1996@ukr.net>
2017-09-18 22:01:13 +0300

    Initialization

---
+++ src/main/java/entity/Project.java
@@ -0,0 +1,47 @@
+package entity;
+
+import java.util.Set;
+
+/**
+ * Created by Jesus on 27.08.2017.
+ */
+public class Project {
+    private int id;
+    private String title;
+    private Set<Employee> employees;
+
+    public Project() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
